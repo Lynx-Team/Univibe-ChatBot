0b8a6b12278c2c524b497d18ae97738f0939e9a8
AndrewGurew <andrewgurew123@gmail.com>
2017-11-08 23:33:42 +1000

    add new api version

--- main.py
+++ main.py
@@ -20,157 +20,61 @@ URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 
 class Command(Enum):
-    HI = 1
-    HELP = 2
-    SUB = 3
-    SEND = 4
-    DEFINE = 5
-    START = 6
-    MYSUBS = 7
-    MYNOT = 8
-    KICK = 9
-    SUBOUT = 10
-    UNKNOWN = 11
-    SHOWTABLE = 12
+    WHATLESSON = "Какая у меня пара?"
+    PROFILE = "Профиль"
+    SEND = "Отправить сообщение"
 
+    CHANGEMOD = "Изменить режить"
+    MYSP = "Мои подписки"
+    MYSPS = "Мои подписчики"
+    BACK = "Назад"
 
-answerTable = {Command.HI: hiMessage, Command.UNKNOWN: unknownMessage, Command.HELP: helpMessage,
-               Command.SUB: subMessage, Command.SEND: sendMessage, Command.START: startMessage,
-               Command.SHOWTABLE: showTableMessage}
+    SUB = "Подписаться"
 
-mod = "s"
+    TTABLE = "Расписание преподавателя"
+    TSTUDENT = "Расписание Cтудента"
+
+def whatLesson():
+    return "Алгебра"
 
 import telebot
 from telebot import types
 bot = telebot.TeleBot("495392477:AAF6ebL1x3bpLKaJqn-t3vP9nNSDHQSvurM")
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    keyboard = types.InlineKeyboardMarkup()
+    # Если сообщение из чата с ботом
+    if call.message:
+        if call.data == "test":
+            bot.send_message(call.message.chat.id, "Я – сообщение из обычного режима", reply_markup=keyboard)
 
 @bot.message_handler(commands=["start"])
-def cmd_start(message):
-    # Создаем клавиатуру и каждую из кнопок (по 2 в ряд)
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-    url_button = types.InlineKeyboardButton(text="URL", url="https://ya.ru")
-    callback_button = types.InlineKeyboardButton(text="Callback", callback_data="test")
-    switch_button = types.InlineKeyboardButton(text="Switch", switch_inline_query="Telegram")
-    keyboard.add(url_button, callback_button, switch_button)
-    bot.send_message(message.chat.id, "Я – сообщение из обычного режима", reply_markup=keyboard)
+def mainMenu(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    keyboard.add(types.KeyboardButton(text=Command.WHATLESSON))
+    keyboard.row_width = 2
+    keyboard.add(types.KeyboardButton(text=Command.PROFILE), types.KeyboardButton(text=Command.SEND))
 
-def handleMessage(text, userName):
-    if (re.match(r'(.*п.*вет|ку|hi|хай|здра.*те)', text, flags=(re.IGNORECASE | re.MULTILINE))):
-        return answerTable[Command.HI]()
-    elif (re.match(r'^\s*(/help|help)\s*$', text)):
-        return answerTable[Command.HELP]()
-    elif (re.match(r'^\s*(/sub|sub)', text)):
-        return answerTable[Command.SUB](text)
-    elif (re.match(r'^\s*(send|/send).*$', text)):
-        return answerTable[Command.SEND](userName, text)
-    elif (re.match(r'^/start$',text)):
-        return answerTable[Command.START]()
-    elif (re.match(r'^(/st|st)$', text)):
-        return answerTable[Command.SHOWTABLE](mod)
-    else:
-        return answerTable[Command.UNKNOWN]()
-
-def createAnswer(updates):
-    answerText = handleMessage(updates["message"]["text"])
-    chat = updates["message"]["chat"]["id"]
-    sendMessage(answerText, chat)
-
-def sendMessage(text, chat_id):
-    text = urllib.parse.quote_plus(text)
-    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
-    getUrl(url)
+@bot.message_handler(commands=["sub"])
+def profile(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    keyboard.add(types.KeyboardButton(text=Command.CHANGEMOD))
+    keyboard.row_width = 2
+    keyboard.add(types.KeyboardButton(text=Command.MYSP), types.KeyboardButton(text=Command.MYSPS))
+    keyboard.row_width = 1
+    keyboard.add(types.KeyboardButton(text=Command.BACK))
 
 
+# @bot.message_handler(content_types=["text"])
+# def any_msg(message):
+#     if(message.text == Command.WHATLESSON):
+#         bot.send_message(message.chat.id, message.text)
+#     if (message.text == Command.SEND):
+#         bot.send_message(message.chat.id, message.text)
+#     if (message.text == Command.PROFILE):
+#         bot.send_message(message.chat.id, message.text)
 
-
-# TEST FUNCTIONS
-
-def getUrl(url):
-    response = requests.get(url)
-    content = response.content.decode("utf8")
-    return content
-
-
-def getJson(url):
-    content = getUrl(url)
-    js = json.loads(content)
-    return js
-
-
-def getUpdates(offset=None):
-    url = URL + "getUpdates"
-    if offset:
-        url += "?offset={}".format(offset)
-    js = getJson(url)
-    return js
-
-
-def createAnswerTest(updates):
-    for update in updates["result"]:
-        text = handleMessage(update["message"]["text"], update["message"]["chat"]["username"])
-        chat = update["message"]["chat"]["id"]
-        sendMessage(text, chat)
-
-
-def getLastUpdateId(updates):
-    update_ids = []
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
-    return max(update_ids)
-
-
-
-def newUser(result):
-    if (len(result) == 0):
-        return
-
-    result = result[0]
-
-    first_name = result['message']['chat']['first_name']
-    chat_id = result['message']['chat']['id']
-
-    print(first_name, chat_id)
-
-    # cursor = cnxn.cursor()
-    # cursor.execute("SELECT * FROM teachers")
-    #where dusers.user_login = N '" + first_name + "'")
-
-
-    # if (not cursor.fetchone()):
-    #     cursor.execute(
-    #         "INSERT INTO dusers(user_login, user_password, last_news_id) values ('" + first_name + "', 'password', null)")
-    #     cursor.commit()
-    #
-    # print(result)
-
-
-def main():
-    last_update_id = None
-    # cursor = cnxn.cursor()
-    # cursor.execute("SELECT * FROM teachers")
-    # cursor.commit()
-    #return
-
-    cursor = cnxn.cursor()
-    cursor.execute("SELECT username FROM AspNetUsers")
-
-    row = cursor.fetchone()
-    while row:
-        print(str(row))
-        row = cursor.fetchone()
-
-    #return
-
-    while True:
-        updates = getUpdates(last_update_id)
-
-        result = updates['result']
-        #newUser(result)
-        if len(result) > 0:
-            last_update_id = getLastUpdateId(updates) + 1
-            createAnswerTest(updates)
-        time.sleep(0.5)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
